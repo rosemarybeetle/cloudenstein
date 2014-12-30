005779b6940571e9bea90a8fa6ec2ecc8b030617
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-12-30 19:43:09 +0000

    adding a looper to format return

--- hello/views.py
+++ hello/views.py
@@ -17,32 +17,39 @@ def index(request):
 	#print r.text
 	#return HttpResponse('<pre>' + r.text + '</pre>')
 	retrieveArray(adminURL)
-    for tt in results:
-        return HttpResponse(results[tt]+'<br/>')
-        #' swCount= '+str (swCount))
+	sendText='' # initialise text sending variable
+	tt=0
+	while tt < swCount:
+		sendText+=results[tt]
+		sendText+='<br />'
+		tt+=1
+	sendText+= 'number of attributes received = '+str (swCount)
+	return HttpResponse(sendText+' !!!<br/>')
+	
+	
 
 def retrieveArray (url):
-    try:
-        Ws= requests.get(url)
-        yy= Ws.text
-        global results
-        results = yy.splitlines()
-        global swCount
-        swCount=len(results)       
-        return (results)
-        return swCount
-    # end retrieveArray
-    except:
-        print ('Can\'t connect to admin settings - no connection') 
+	try:
+		Ws= requests.get(url)
+		yy= Ws.text
+		global results
+		results = yy.splitlines()
+		global swCount
+		swCount=len(results)       
+		return (results)
+		return swCount
+	# end retrieveArray
+	except:
+		print ('Can\'t connect to admin settings - no connection') 
 
 
 
 def db(request):
 
-    greeting = Greeting()
-    greeting.save()
+	greeting = Greeting()
+	greeting.save()
 
-    greetings = Greeting.objects.all()
+	greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+	return render(request, 'db.html', {'greetings': greetings})
 
