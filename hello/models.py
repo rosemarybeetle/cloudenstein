7d3738210b8c989af63ee51704db2d6f314374cf
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-10 11:17:11 +0000

    error catching

--- hello/models.py
+++ hello/models.py
@@ -5,7 +5,7 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class lastTweetId(models.Model):
-	last_tweet_id = models.IntegerField()
+	last_tweet_id = models.IntegerField(max_length=20)
 
 class LT(models.Model):
 	lt_id=models.BigIntegerField()
