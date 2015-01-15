cecde40b9d6c58b5f9f70e1e255f87dc672387c3
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-15 00:02:41 +0000

    not globally declared

--- hello/views.py
+++ hello/views.py
@@ -61,6 +61,8 @@ def index(request):
 
 def loadAdminSettings ():
 	adminSettings = cloud_admin.objects.filter(id=0)
+	global t_st_ad
+	global t_sn_ad
 	t_st_ad=adminSettings[0].search_term
 	t_sn_ad=adminSettings[0].tweet_num
 	return (t_st_ad,t_sn_ad)
