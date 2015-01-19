4b814caa57eb097af221d7afdfcb3fd6c8dfe514
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-19 19:57:48 +0000

    typo

--- hello/models.py
+++ hello/models.py
@@ -19,7 +19,7 @@ class lt_st(models.Model):
 class tweeter(models.Model):
 	tid=models.CharField(max_length=20)
 	name=models.CharField(max_length=140)
-	username=models.CharField(max_length=12)
+	username=models.CharField(max_length=100)
 	status=models.CharField(max_length=140)
 	avatar=models.CharField(max_length=140)
 
