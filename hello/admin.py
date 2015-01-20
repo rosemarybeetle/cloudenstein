07dae42cd36783ac5b0f340d90569a9af4802b85
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-20 19:28:51 +0000

    update tweeten in  admin

--- hello/admin.py
+++ hello/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from .models import cloud_admin
 from .models import process_settings
 from .models import stop_words
-from .models import tweeter
+from .models import tweeten
 
 admin.site.register(cloud_admin)
 admin.site.register(process_settings)
 admin.site.register(stop_words)
-admin.site.register(tweeter)
+admin.site.register(tweeten)
 # Register your models here.
 
