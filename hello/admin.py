bb5c0f67d8ca6d8e8d29d98117bd7dca3aa30db1
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-21 18:51:46 +0000

    add lt_st to admin

--- hello/admin.py
+++ hello/admin.py
@@ -4,10 +4,12 @@ from .models import cloud_admin
 from .models import process_settings
 from .models import stop_words
 from .models import tweeten
+from .models import lt_st
 
 admin.site.register(cloud_admin)
 admin.site.register(process_settings)
 admin.site.register(stop_words)
 admin.site.register(tweeten)
+admin.site.register(lt_st)
 # Register your models here.
 
