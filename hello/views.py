3132818a6c7ed1673c41b255be51ad6639158694
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-01-27 18:44:51 +0000

    typo

--- hello/views.py
+++ hello/views.py
@@ -309,7 +309,8 @@ def oculus (request):
 			hashtag_destroyer[i].delete()
 		except:
 			conter+=1
-	return ('Failed to destroy all rows. '+ str(conter)+' rows of 500 failed' )
+	mein='Failed to destroy all rows. '+ str(conter)+' rows of 500 failed'
+	return mein
 
 
 	return render (request, 'oculus.html')
