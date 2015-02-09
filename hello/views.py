da1e84126add26becf59d5e493939d52cc8fdf8a
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-02-09 00:12:17 +0000

    stop last 20 returning

--- hello/views.py
+++ hello/views.py
@@ -399,9 +399,10 @@ def recent_mentions(request):
 		save_last_mention(ting[0]['id'], ting[0]['user']['screen_name'])
 		responsetext='[{"message":"'+str(ting_len)+' new mentions retrieved."{'
 		for y in range (0 , ting_len):
-			responsetext+='"status_id":"'+str(ting[y]['id'])+'","name":"'+str(ting[y]['user'])+'","screen_name":"'+str(ting[y]['user']['screen_name'])+'"'
-			if ting_len-y>0:
-				responsetext+="'"
+			if last_mention_now > ting[y]['id']:
+				responsetext+='"status_id":"'+str(ting[y]['id'])+'","name":"'+str(ting[y]['user'])+'","screen_name":"'+str(ting[y]['user']['screen_name'])+'"'
+				if ting_len-y>0:
+					responsetext+="'"
 		responsetext+='}}]'
 	else:
 		responsetext='[{"message":"if last_mention_now > lt_mn_id: FAILED"}]'
