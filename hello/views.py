4665b6832e9d55b313da36f93c3890843b1f932d
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-02-10 20:45:04 +0000

    yep. forgot the zeroth offset

--- hello/views.py
+++ hello/views.py
@@ -405,7 +405,7 @@ def recent_mentions(request):
 		for y in range (0 , h): 
 			if lt_mn_id < int(ting[y]['id']):
 				temptext+='{"h":"'+str(h)+'","y":"'+str(y)+'","ting_len":"'+str(ting_len)+'","status_id":"'+str(ting[y]['id'])+'","name":"'+str(ting[y]['user']['name'])+'","screen_name":"'+str(ting[y]['user']['screen_name'])+'"}'
-				if h>y:
+				if h>y+1:
 					temptext+=","
 				responsetext='{"message":"'+str(y+1)+' new mentions retrieved.","mentions":['
 		responsetext+=temptext+']}'
