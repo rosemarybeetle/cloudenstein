bd89428df8b28da30b68e513c26b4e570d728989
Rosemary Beetle <rosemarybeetle@gmail.com>
2015-04-25 15:57:17 +0100

    typo

--- hello/views.py
+++ hello/views.py
@@ -556,9 +556,9 @@ def logon (request):
 	loadAdminSettings()
 	return render (request, 'logon.html',{'period':t_hp_ad})
 
-def harvester (request):
-	loadAdminSettings()
-	return render (request, 'harvester.html',{'period':t_hp_ad})
+# def harvester (request):
+# 	loadAdminSettings()
+# 	return render (request, 'harvester.html',{'period':t_hp_ad})
 	
 
 def fpi (request):
